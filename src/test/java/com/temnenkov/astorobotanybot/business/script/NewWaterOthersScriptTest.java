c15f96c10ab2a7772dce34876bd8af1a59dce54c
Kirill Temnenkov <kirill@temnenkov.com>
2023-09-11 22:30:15 +0300

    water others script implemented, next step - test it

---
+++ src/test/java/com/temnenkov/astorobotanybot/business/script/NewWaterOthersScriptTest.java
@@ -0,0 +1,26 @@
+package com.temnenkov.astorobotanybot.business.script;
+
+import org.junit.jupiter.api.Test;
+
+class NewWaterOthersScriptTest {
+
+    @Test
+    void invoke() {
+        //todo implement
+    }
+
+    @Test
+    void processGarden() {
+        //todo implement
+    }
+
+    @Test
+    void waterByPriority() {
+        //todo implement
+    }
+
+    @Test
+    void loadPlantsToWater() {
+        //todo implement
+    }
+}
