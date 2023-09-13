320d275a064f064fbd95cca7a8bb251d3690fcfa
Kirill Temnenkov <kirill.temnenkov@mts.ru>
2023-09-13 21:52:48 +0300

    start implement test of newwaterscript

--- src/test/java/com/temnenkov/astorobotanybot/business/script/NewWaterOthersScriptTest.java
+++ src/test/java/com/temnenkov/astorobotanybot/business/script/NewWaterOthersScriptTest.java
@@ -1,26 +1,42 @@
 package com.temnenkov.astorobotanybot.business.script;
 
-import org.junit.jupiter.api.Test;
+import com.temnenkov.astorobotanybot.business.GameClient;
+import com.temnenkov.astorobotanybot.business.parser.GardenParser;
+import com.temnenkov.astorobotanybot.business.parser.dto.GardenPageState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
 class NewWaterOthersScriptTest {
 
-    @Test
-    void invoke() {
-        //todo implement
+    private NewWaterOthersScript script;
+
+    @Mock
+    private GameClient gameClient;
+    @Mock
+    private NewWaterOthersScriptWorker newWaterOthersScriptWorker;
+    @Mock
+    private GardenParser gardenParser;
+
+    @BeforeEach
+    void setUp() {
+        script = new NewWaterOthersScript(gameClient, newWaterOthersScriptWorker, gardenParser);
     }
 
-    @Test
-    void processGarden() {
-        //todo implement
-    }
-
-    @Test
-    void waterByPriority() {
-        //todo implement
-    }
-
-    @Test
-    void loadPlantsToWater() {
-        //todo implement
+    //@Test
+    void wilting() {
+        //given
+        Mockito.when(gameClient.myPlant()).thenReturn("1");
+        Mockito.when(gardenParser.parse("1")).thenReturn(new GardenPageState(
+                Map.of("1", "2"), null
+        ));
+        //when
+        script.invoke();
+        //then
     }
 }
