2f2becbaff8791d1e0c452e999eb3ad146db314f
Kirill Temnenkov <kirill.temnenkov@mts.ru>
2023-09-14 22:33:51 +0300

    test for wilting others

--- src/test/java/com/temnenkov/astorobotanybot/business/script/NewWaterOthersScriptTest.java
+++ src/test/java/com/temnenkov/astorobotanybot/business/script/NewWaterOthersScriptTest.java
@@ -4,12 +4,16 @@ import com.temnenkov.astorobotanybot.business.GameClient;
 import com.temnenkov.astorobotanybot.business.parser.GardenParser;
 import com.temnenkov.astorobotanybot.business.parser.dto.GardenPageState;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class NewWaterOthersScriptTest {
@@ -28,15 +32,20 @@ class NewWaterOthersScriptTest {
         script = new NewWaterOthersScript(gameClient, newWaterOthersScriptWorker, gardenParser);
     }
 
-    //@Test
+    @Test
     void wilting() {
         //given
-        Mockito.when(gameClient.myPlant()).thenReturn("1");
-        Mockito.when(gardenParser.parse("1")).thenReturn(new GardenPageState(
+        when(gameClient.wiltingPlants()).thenReturn("1");
+        final GardenPageState gardenPageState = new GardenPageState(
                 Map.of("1", "2"), null
-        ));
+        );
+        when(gardenParser.parse("1")).thenReturn(gardenPageState);
+        final NewWaterOtherScriptResult.Watered expectedResult = new NewWaterOtherScriptResult.Watered(10, 100);
+        when(newWaterOthersScriptWorker.processGarden(gardenPageState)).thenReturn(expectedResult);
         //when
-        script.invoke();
+        final NewWaterOtherScriptResult result = script.invoke();
         //then
+        assertEquals(expectedResult, result);
+        verifyNoMoreInteractions(gameClient, newWaterOthersScriptWorker, gardenParser);
     }
 }
